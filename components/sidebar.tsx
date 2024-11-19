7c0f85ebaabb54d89bab776bf774ac55e7f9ae13
Rehanjoy <rehan101err0r@gmail.com>
2024-11-20 01:37:10 +0530

    new update

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -24,7 +24,7 @@ export default function Sidebar() {
       <motion.div
         onMouseEnter={() => setIsHovered(true)}
         onMouseLeave={() => setIsHovered(false)}
-        className={`fixed top-0 left-0  bg-black border-r border-gray-800 flex flex-col py-6 transition-all duration-300 z-40 ${
+        className={`fixed top-0 left-0  bg-black flex flex-col py-6 transition-all duration-300 z-40 ${
           isHovered || isMobileOpen ? 'w-44 min-h-screen' : 'w-16 min-h-24 rounded-2xl '
         } ${isMobileOpen ? 'lg:hidden' : ''}`}
       >
